ebb482ea8232e26d9e8598edb38ba9df82d15d6c
darkweb19 <sujanstha3220@gmail.com>
2023-02-22 18:54:12 +0545

    added the js for sum

--- Day2/first.js
+++ Day2/first.js
@@ -1,1 +1,6 @@
-
+function add() {
+  let num1 = parseInt(document.getElementById("num1").value);
+  let num2 = parseInt(document.getElementById("num2").value);
+  let sum = num1 + num2;
+  document.getElementById("sum").innerHTML = "sum is " + sum;
+}
