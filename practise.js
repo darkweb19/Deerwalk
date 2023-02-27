2a28339c50a2fdd315611d7338d2046339f8e0ed
darkweb19 <sujanstha3220@gmail.com>
2023-02-27 17:27:18 +0545

    added day 4 codes

--- practise.js
+++ practise.js
@@ -291,3 +291,17 @@ function Potatoes(str) {
 	console.log(counter);
 }
 // Potatoes("potatoaspotato potato as potato"); // output : 4
+
+//!Number Splits :
+/*
+Given a number, return an array containing the two halves of the number. If the number is odd, make the rightmost number higher.
+Examples :
+
+numberSplit(4) ➞ [2, 2]
+numberSplit(10) ➞ [5, 5]
+numberSplit(11) ➞ [5, 6]
+numberSplit(-9) ➞ [-5, -4]
+*/
+function NumberSplit(num) {
+	let arr = [];
+}
