c8ac183bbcf240c20a2fd9c08dbb0f3e863dc581
darkweb19 <sujanstha3220@gmail.com>
2023-03-05 20:49:30 +0545

    added more in practisejs

--- practise.js
+++ practise.js
@@ -328,15 +328,16 @@ function minMax(arr) {
 // console.log(minMax([1]));
 
 //!Number Splits Given a number, return an array containing the two halves of the number. If the number is odd, make the rightmost number higher.
+/*
+numberSplit(4) ➞ [2, 2]
 
-// Examples
-// numberSplit(4) ➞ [2, 2]
+numberSplit(10) ➞ [5, 5]
 
-// numberSplit(10) ➞ [5, 5]
+numberSplit(11) ➞ [5, 6]
 
-// numberSplit(11) ➞ [5, 6]
-
-// numberSplit(-9) ➞ [-5, -4]
+numberSplit(-9) ➞ [-5, -4]
+*/
+Examples;
 function numberSplit(n) {
 	let arr = [];
 	if (n % 2 == 0) {
