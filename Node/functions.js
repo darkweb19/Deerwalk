e5cc9d64bfe40b32f0d270fc0009406ddc86c9fa
darkweb19 <sujanstha3220@gmail.com>
2023-03-07 18:29:10 +0545

    Added function js

--- Node/functions.js
+++ Node/functions.js
@@ -1,4 +1,16 @@
 const sayHi = require("./index"); // this is my module
 const http = require("http");
-
-sayHi();
+const server = http.createServer((req, res) => {
+	// console.log(req.url);
+	if (req.url == "/") {
+		res.write("Hello world");
+	} else if (req.url == "/home") {
+		res.write("Welcome to Homepage");
+	} else {
+		res.write("404 not found");
+	}
+	res.end();
+});
+server.listen(3000, () => {
+	console.log("Listening on port 3000");
+});
