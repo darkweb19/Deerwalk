ac1896c0b27a03333ea6838c457bda63973d44cf
darkweb19 <sujanstha3220@gmail.com>
2023-03-07 16:49:38 +0545

    added index and first js in Node dir

---
+++ Node/functions.js
@@ -0,0 +1,4 @@
+const sayHi = require("./index"); // this is my module
+const http = require("http");
+
+sayHi();
---
+++ Node/index.js
@@ -0,0 +1,6 @@
+//!First node class
+function hi() {
+	console.log("Hiii");
+}
+
+module.exports = hi;
